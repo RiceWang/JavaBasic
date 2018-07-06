b040c006a707f334442ab8d71a3bd11985ed1184
wangf <wangfan6134@sina.com>
2018-07-06 17:32:38 +0800

    测试github提交

---
+++ DesignMode/src/main/java/cn/wang/mode/observer/Subject.java
@@ -0,0 +1,5 @@
+package cn.wang.mode.observer;
+
+public interface Subject {
+
+}
---
+++ DesignMode/src/main/java/cn/wang/mode/observerImpl/Subject1.java
@@ -0,0 +1,7 @@
+package cn.wang.mode.observerImpl;
+
+import cn.wang.mode.observer.Subject;
+
+public class Subject1 implements Subject {
+
+}
